28675bd053e1fdae3cb80307bc4f5723c1812f75
馬場義弘 <babayoshihiro@YB-MBP14-2.local>
2025-06-11 20:51:48 +0900

    First commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+const jsHamburger = document.getElementById('js-hamburger');
+const Body = document.body;
+// const jsGlobalMenu = document.getElementById('js-global-menu');
+const hamburgerList = document.querySelector('.c-hamburger')
+jsHamburger.addEventListener('click', function(){
+    // Body
+    if (this.getAttribute('aria-expanded') == 'false') {
+        this.setAttribute('aria-expanded', true)
+        hamburgerList.style.visibility = 'visible'
+        Body.style.overflow = 'hidden'
+        jsHamburger.setAttribute('aria-hidden', false)
+    }else{
+        this.setAttribute('aria-expanded', false)
+        hamburgerList.style.visibility = 'hidden'
+        jsHamburger.setAttribute('aria-hidden', 'true')
+        Body.style.overflow = "visible";
+    }
+});
